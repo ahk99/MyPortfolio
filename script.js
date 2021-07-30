cb2348cbe6784f47d20c57bbe07e6ba56ea2cc9b
ahk99 <53700354+ahk99@users.noreply.github.com>
2021-07-30 11:24:45 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+function showIt() {
+    document.getElementById("abc").style.display = "inline-block";
+}
+setTimeout("showIt()", 3500);
+
+function shownofl() {
+    document.getElementById("flex").style.display = "none";
+}
+setTimeout("shownofl()", 3500)
