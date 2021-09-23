25fecc6658ac6154d4a522c59db67fb044c5da94
ahk99 <53700354+ahk99@users.noreply.github.com>
2021-09-23 11:02:36 +0530

    javascript file

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-smoothscroll.init();
+function validateform() {
+    let x = document.forms["myform"]["name"].value;
+
+    if (isNaN(x)) {
+        return true
+        alert("Your form has been submitted. Thank you!!")
+    } else {
+        alert("enter a valid name");
+        return false;
+    }
+}
+
+x = 1
+
+function shownofmenu() {
+    if (x == 1) {
+        document.getElementById("menuicon").style.visibility = "visible";
+        document.getElementById("flb").style.visibility = "visible";
+
+
+        x = 0
+    } else {
+        document.getElementById("menuicon").style.visibility = "hidden";
+        document.getElementById("flb").style.visibility = "hidden";
+        x = 1
+
+    }
+}
